b0e3264261b8c72ad0e6a87b904093675e18ffc3
Ontida Tanthmanatham <ontidaxt@gmail.com>
2016-06-10 23:36:12 -0700

    Connected index.html, alice-app.js, and slice-server.js. code handling only one result response at the moment

--- alice-index-src/alice-app.js
+++ alice-index-src/alice-app.js
@@ -1,22 +1,56 @@
 
+var selected = function(collection) {
+	//var category = document.getElementById('add-text'); 
+	var responseObj = JSON.parse(collection);
+	
+	var store = responseObj.businesses[0];
+	var storeName = store.name;
+	var storeUrl = store.url;
+	var storeCategories = store.categories;
+	var storeAddress = store.location.address[0] + ', '+ store.location.city + ', ' + store.location['state_code'] + ', ' + store.location['postal_code']
+	//console.log(store.location['state_code']);
+	//console.log(store.location['postal_code']);
+	var storeCrossStreet = store.location['cross_streets'];
+	var storeRatingImg = store['rating_img_url_small'];
 
-//var search = function() {
+	var businessName = document.getElementById('business-name'); 
+	businessName.innerText = storeName;
+	//var category = document.getElementById('categories');
+	//category.innerText = storeCategories;
+	var businessRating = document.getElementById('rating-img');
+	businessRating.src = storeRatingImg;
+	var businessAddress = document.getElementById('address');
+	businessAddress.innerText = storeAddress;
+	
+}
 
+console.log("alice-app.js is connected!");
+
+var search = function() {
+	var location = document.getElementById("zipcode");
+	var zipcode = location.value;
+	
+
+	//var zipcode = "94102";
 
 	var xhr = new XMLHttpRequest();
 
-	xhr.open('GET',"/api/", true);
+	xhr.open('GET',"/api/search?zip=" + zipcode);
 
 	xhr.onload = function(){	
 	  if(xhr.status === 200){
-	    selected(xhr.responseText);
+		selected(xhr.responseText);
 	  } else {
 		alert("The alice-app.js request failed. Return the status of "+ xhr.resposeText);
-		return;
 	  }
 
 	};
 
+
 	xhr.send();
 
-//};
+}
+
+//google.com/search?value=cats&date=4102015
+
+
